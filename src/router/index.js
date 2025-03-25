28b15f4ea17c22c99245a8b0539e35ce0258e3f3
bidianqing <bidianqing@qq.com>
2025-03-25 16:57:39 +0800

    *

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@/layout/index.vue'
 import HelloWorld from '@/components/HelloWorld.vue'
-import Index from '@/views/index.vue'
-import About from '@/views/about.vue'
 import NotFound from '@/views/404.vue'
-import Login from '@/views/login.vue'
-import News from '@/views/news.vue'
 import Jobs from '@/views/jobs.vue'
 import Triggers from '@/views/triggers.vue'
 
@@ -26,36 +22,12 @@ const routes = [
         component: NotFound
     },
     {
-        path: '/login',
-        component: Login
-    },
-    {
         path: '/hello',
         component: Layout,
         children: [
             {
               path: '',
               component: HelloWorld
-            }
-        ]
-    },
-    {
-        path: '/about',
-        component: Layout,
-        children: [
-            {
-              path: '/about',
-              component: About
-            }
-        ]
-    },
-    {
-        path: '/news',
-        component: Layout,
-        children: [
-            {
-              path: '/news',
-              component: News
             }
         ]
     },
